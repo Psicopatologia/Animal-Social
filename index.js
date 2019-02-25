067f53d1689fa10d0793ca78eafa4fdacab71dbc
Sebastian Orozco <sebastianorozcogomez@gmail.com>
2019-02-25 14:26:38 -0500

    Se conectó a base de datos

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/index')
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 // Settings
 app.set('port', process.env.PORT || 3000);
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs')
 
-
 // Routes
 app.use(routes);
+
+// Database conection url
+const url = 'mongodb://localhost:27017';
+
+// Database name
+const dbName = 'ASdb';
+
+// Conect to mongodb server
+MongoClient.connect(url,{ useNewUrlParser: true },(err,client) => {
+    assert.equal(null, err);
+    console.log("Connected successfully to database server");
+
+    const db = client.db(dbName);
+
+    client.close();
+})
 
 // Static files
 app.use(express.static('public'));
