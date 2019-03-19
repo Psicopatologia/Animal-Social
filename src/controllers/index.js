ab2a70c4adb0766b3acdf84cea34b903d5eba4b1
Sebastian Orozco <sebastianorozcogomez@gmail.com>
2019-03-19 16:39:05 -0500

    Se modifico la forma en la que se accede a los perfiles

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,37 +1,40 @@
+const User = require('../models/User');
 const passport = require('passport');
 
-const index = (req, res) => {
-    res.render('pages/index')
-};
+// Get
+const index = (req, res) => { res.render('pages/index') };
 
-const signupGet = (req, res) => {
-    res.render('pages/signup')
-};
+const signupGet = (req, res) => { res.render('pages/signup') };
 
-const signupPost = passport.authenticate('local_signup', {
-    successRedirect: '/',
-    failureRedirect: '/signup',
-    passReqToCallback: true
-});
-
-const signinGet = (req, res) => {
-    res.render('pages/signin');
-};
-
-const signinPost = passport.authenticate('local_signin', {
-    successRedirect: '/',
-    failureRedirect: '/signin',
-    passReqToCallback: true
-});
+const signinGet = (req, res) => { res.render('pages/signin'); };
 
 const logout = (req, res) => {
     req.logout();
     res.redirect('/');
 };
 
-const profile = (req, res) => {
-    res.render('pages/profile')
+const profiles = (req, res, next) => {
+    let userN = req.params.userName;
+    User.findOne({'userName': userN}, 'email', (err, user) => {
+        if (user)
+            res.render('pages/profile', user);
+        else
+            next();
+    });
 }
+
+// Post
+const signupPost = passport.authenticate('local_signup', {
+    successRedirect: '/',
+    failureRedirect: '/signup',
+    passReqToCallback: true
+});
+
+const signinPost = passport.authenticate('local_signin', {
+    successRedirect: '/',
+    failureRedirect: '/signin',
+    passReqToCallback: true
+});
 
 module.exports = {
     index,
@@ -40,5 +43,5 @@ module.exports = {
     signinGet,
     signinPost,
     logout,
-    profile
+    profiles
 }
