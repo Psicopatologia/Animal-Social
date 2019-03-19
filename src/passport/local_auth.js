636b3e8688e249abefb3e178c6a201acfa2c80af
Sebastian Orozco <sebastianorozcogomez@gmail.com>
2019-03-19 11:28:44 -0500

    Se modificó el metodo de signup para incluir nombre de usuario

--- src/passport/local_auth.js
+++ src/passport/local_auth.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
-const User = require('../models/user');
+const User = require('../models/User');
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
@@ -24,6 +24,7 @@ passport.use('local_signup', new LocalStrategy({
     } else {
         const newUser = new User();
         newUser.email = email;
+        newUser.userName = newUser.id;
         newUser.password = newUser.encryptPassword(password);
         await newUser.save();
         done(null, newUser);
