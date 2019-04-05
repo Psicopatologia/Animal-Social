12c5c44276c7a4a8e9c0fd8595c7334d221a90ad
Sebastian Orozco <sebastianorozcogomez@gmail.com>
2019-04-04 19:23:34 -0500

    Se añadió i18n y cookie parser

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const passport = require('passport');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cookieParser = require('cookie-parser');
+const i18n = require('i18n');
 
 // Initializations
 const app = express();
@@ -13,9 +15,16 @@ require('./passport/local_auth');
 // Settings
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+i18n.configure({
+    locales:['en', 'es'],
+    defaultLocale: 'en',
+    cookie: 'lang',
+    directory: path.join(__dirname, 'locales'),
+});
 
 // Middlewares
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
     secret: 'AnimalSocialSessionXD',
@@ -25,7 +34,7 @@ app.use(session({
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use(i18n.init);
 app.use((req, res, next) => {
     app.locals.signupMessage = req.flash('signupMessage');
     app.locals.signinMessage = req.flash('signinMessage');
